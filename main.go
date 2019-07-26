66c8470de16d63e460749c11d496d633cf113f97
Tin Trinh <trinh.tin@hamee.co.jp>
2019-07-26 15:08:40 +0900

    move read write to report package for reuse
    refs #57284

--- main.go
+++ main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 )
 
 func check(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// ListDir list all file in the dir
+func ListDir(dir string) ([]string, error) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	filePaths := []string{}
+	for _, file := range files {
+		if !file.IsDir() {
+			filePaths = append(filePaths, dir+"/"+file.Name())
+		}
+	}
+
+	return filePaths, nil
 }
 
 func main() {
